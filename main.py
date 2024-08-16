276ad0aedf3f167db5f3d45026a3a3cb3ef799b2
Gopi2466 <Gopi2466@gmail.com>
2024-08-16 02:39:31 -0400

    initial grid sample

---
+++ main.py
@@ -0,0 +1,16 @@
+# this is the code for the cli tictactoe game.
+#Todo:
+
+class Grid:
+    def __init__(self):
+        #logic of how to implement a grid.
+        """
+        x|x|x
+        o|o|o
+        x|x|X
+        
+        _|_|_
+        _|_|_
+         | |
+        """
+        pass
